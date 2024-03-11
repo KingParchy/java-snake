a6dc185febe4ebe4557f7cc9dda0ad324bc4c087
Jeremy Parchment <jparch@live.co.uk>
2024-03-11 21:08:50 +0900

    Added keyboard controls for the snake game.

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-
-public class SnakeGame extends JPanel {
+public class SnakeGame extends JPanel implements ActionListener, KeyListener {
     private class Tile {
         int x , y;  // coordinates of the tile in the grid
 
@@ -19,15 +18,38 @@ public class SnakeGame extends JPanel {
     int boardHeight;
     int tileSize = 32;
 
+    // Snake
     Tile snakeHead;
+
+    // Food
+    Tile food;
+    Random random;
+
+    // game logic
+    Timer gameLoop;
+    int velocityX;
+    int velocityY;
 
     SnakeGame(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
         setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
         setBackground(Color.black);
+        addKeyListener(this);
+        setFocusable(true);
 
         snakeHead = new Tile(5, 5);
+
+        food = new Tile(10, 10);
+        random = new Random();
+        placeFood();
+
+        // Initial
+        velocityX = 0;
+        velocityY = 0;   
+
+        gameLoop = new Timer(100, this);
+        gameLoop.start();
 
     }
 
@@ -43,8 +65,58 @@ public class SnakeGame extends JPanel {
             g.drawLine(i*tileSize, 0, i*tileSize, boardHeight);
             g.drawLine(0, i*tileSize, boardWidth, i*tileSize);
         }
+
+        // Food
+        g.setColor(Color.RED);
+        g.fillRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize);
+
         // Snake
         g.setColor(Color.GREEN);
         g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
+    }
+
+    public void placeFood() {
+        food.x = random.nextInt(boardWidth/tileSize);
+        food.y = random.nextInt(boardHeight/tileSize);
+    }
+
+    public void move() {
+        // Snake Head
+        snakeHead.x += velocityX;
+        snakeHead.y += velocityY;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+       if (e.getKeyCode() == KeyEvent.VK_UP && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+       } else if (e.getKeyCode() == KeyEvent.VK_DOWN && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+       } else if (e.getKeyCode() == KeyEvent.VK_LEFT && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+       } else if (e.getKeyCode() == KeyEvent.VK_RIGHT && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+       }
+    }
+
+    // do not need
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        
     }
 }
